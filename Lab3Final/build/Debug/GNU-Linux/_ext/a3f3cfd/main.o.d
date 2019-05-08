fac7c0c5c55c5f3fc51bc3f17b449195c7eba623
yuxincheng73 <50343180+yuxincheng73@users.noreply.github.com>
2019-05-08 01:13:47 -0400

    Add files via upload

---
+++ Lab3Final/build/Debug/GNU-Linux/_ext/a3f3cfd/main.o.d
@@ -0,0 +1,6 @@
+build/Debug/GNU-Linux/_ext/a3f3cfd/main.o: ../lab3_release/main.cpp \
+ ../lab3_release/globals.h ../lab3_release/shape.h
+
+../lab3_release/globals.h:
+
+../lab3_release/shape.h:
---
+++ Lab3Final/build/Debug/GNU-Linux/_ext/a3f3cfd/shape.o.d
@@ -0,0 +1,4 @@
+build/Debug/GNU-Linux/_ext/a3f3cfd/shape.o: ../lab3_release/shape.cpp \
+ ../lab3_release/shape.h
+
+../lab3_release/shape.h:
